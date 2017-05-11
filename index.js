2dac6680bda103921b496394d4eb3e396392bce7
alecglassford <glassford@stanford.edu>
2017-05-11 15:35:40 -0700

    Make serving static files just a wee bit more robust

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var path = require('path');
 var express = require('express');
+
 var app = express();
 
-app.use(express.static('app'));
+app.use(express.static(path.join(__dirname, 'app')));
 
 app.listen(8000, function () {
   console.log('Listening on port 8000!')
