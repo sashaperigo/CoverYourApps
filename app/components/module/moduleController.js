ef4771fa3bade5ab935ad79cc7ef5a37a190734c
Shirin Salehi <shirinsalehi@DN51ul1k.SUNet>
2017-05-27 19:31:17 -0700

    displays slide text

--- app/components/module/moduleController.js
+++ app/components/module/moduleController.js
@@ -5,6 +5,11 @@ myApp.controller('ModuleController', ['$scope', '$rootScope', '$location',
         $scope.module = {};
         $scope.module.name = "";
         $scope.module.pageNumber = 0;
+        $scope.module.json = "";
+        $scope.module.scripts = {};
+        $scope.module.sectionNames = {};
+        $scope.module.section = "";
+        $scope.module.length = 10;
 
         $scope.$on('$viewContentLoaded', function() {
             if ($location.path().includes('auth')) {
@@ -24,7 +29,34 @@ myApp.controller('ModuleController', ['$scope', '$rootScope', '$location',
         	}
         	$scope.module.pageNumber--;
         };
-
+        
+        $scope.module.setSection = function(clicked) {
+            $scope.module.section = clicked;
+            $scope.module.length = module.scripts[$scope.module.section].length;
+        }
+        
+        $scope.main.FetchModel = function(url, callback) {
+            var data;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                    callback(JSON.parse(xmlhttp.responseText));
+                }
+            };
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+        };
+        $scope.callback = function(data) {
+            $scope.$apply(function () {
+                $scope.module.json = data;
+                for(var i = 0; i < data.length; i++) {
+                    $scope.module.sectionNames[i] = data[i].sectionName;
+                    $scope.module.scripts[data[i].sectionName] = data[i].slides;
+                }
+            });
+        };
+        $scope.main.FetchModel("/module_text/phishing.json", $scope.callback);
+        
         $scope.module.incrementPage = function() {
         	if ($scope.module.pageNumber >= $scope.module.maxPages) {
         		return;
