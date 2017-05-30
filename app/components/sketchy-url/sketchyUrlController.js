79f66e42c9376709440af48550ba48a298d9b7bb
alecglassford <glassford@stanford.edu>
2017-05-30 16:28:13 -0700

    Get embedded sketchurl basicly loaded

--- app/components/sketchy-url/sketchyUrlController.js
+++ app/components/sketchy-url/sketchyUrlController.js
@@ -4,12 +4,14 @@ var SketchyUrlController = myApp.controller('SketchyUrlController', ['$scope', '
     function($scope, $rootScope) {
         $scope.sketchyUrl = {};
         $scope.sketchyUrl.name = "Hi!";
+        console.log('loaded sketchyurl');
+        console.log($scope.main);
     }
 ]);
 
 myApp.component('sketchyUrl', {
     templateUrl: '/components/sketchy-url/sketchyUrl.html',
-    controller: SketchyUrlController,
+    controller: 'SketchyUrlController',
     bindings: {
         module: '<'
     }
