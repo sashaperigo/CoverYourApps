98621572d4de19246d1433d6aa2da1dd9554bc4b
Sasha Perigo <sasha.perigo@gmail.com>
2017-06-01 10:33:04 -0700

    Fixing scope on sketchy bcc

--- app/components/sketchy-bcc/sketchyBccController.js
+++ app/components/sketchy-bcc/sketchyBccController.js
@@ -4,9 +4,9 @@ var SketchyBccController = myApp.controller('SketchyBccController', ['$scope', '
     function($scope, $rootScope, $mdDialog) {
         var ctrl = this;
         $scope.sketchyBcc = {};
-        $scope.content = ctrl.slide.emailData;
+        $scope.sketchyBcc.content = ctrl.slide.emailData;
 
-        $scope.showCorrect = function(ev) {
+        $scope.sketchyBcc.showCorrect = function(ev) {
         // Appending dialog to document.body to cover sidenav in docs app
         // Modal dialogs should fully cover application
         // to prevent interaction outside of dialog
@@ -22,7 +22,7 @@ var SketchyBccController = myApp.controller('SketchyBccController', ['$scope', '
         );
       };
 
-      $scope.showIncorrect = function(ev) {
+      $scope.sketchyBcc.showIncorrect = function(ev) {
       // Appending dialog to document.body to cover sidenav in docs app
       // Modal dialogs should fully cover application
       // to prevent interaction outside of dialog
