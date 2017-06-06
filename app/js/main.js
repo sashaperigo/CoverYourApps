7284292d13adb48f46ae901a324508152eafec78
emilyxie <emilyxie@stanford.edu>
2017-06-06 11:31:52 -0700

    fixing learn button

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,18 @@
 function initEmphasizeButton(){
 	$('.emphasize').hover(function(e){
-		$(e.target).children().css( "color", "#ffffff" )
+		if(! $(e.target).is('a')){
+			$(e.target).children().css( "color", "#ffffff" );
+		}
 	}, function(e){
-		$(e.target).children().css( "color", "#851e1e" )
+		if(! $(e.target).is('a')){
+			$(e.target).children().css( "color", "#851e1e" );
+		}
+	});
+
+	$('.emphasize').click(function(e){
+		if(! $(e.target).is('a')){
+			window.location.href = $($(e.target).find('a')).prop('href');
+		}
 	});
 }
 
