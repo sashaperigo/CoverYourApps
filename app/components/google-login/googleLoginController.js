811b19743a27130a5d35a9059c3ac68b85fcceb9
Sasha Perigo <sasha.perigo@gmail.com>
2017-06-10 02:20:48 -0700

    Working on Google login styles

--- app/components/google-login/googleLoginController.js
+++ app/components/google-login/googleLoginController.js
@@ -1,7 +1,19 @@
 'use strict';
 
 var GoogleLogin = myApp.controller('GoogleLoginController', ['$scope', '$rootScope',
-    function($scope, $rootScope) {}
+    function($scope, $rootScope) {
+        $scope.googleLogin = {};
+        $scope.googleLogin.username = '';
+        $scope.googleLogin.error = false;
+
+        $scope.googleLogin.login = function() {
+            if ($scope.googleLogin.username === '') {
+                $scope.googleLogin.error = true;
+            } else {
+                $scope.googleLogin.error = true;
+            }
+        };
+    }
 ]);
 
 myApp.component('googleLogin', {
