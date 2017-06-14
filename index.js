94cb537a456ff46f0ca2d9ac95fa3f78d484a4ce
alecglassford <glassford@stanford.edu>
2017-06-13 22:36:13 -0700

    Force 👏 https 👏 on 👏 Heroku - fixes cookie issues

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 var path = require('path');
 var express = require('express');
+var sslRedirect = require('heroku-ssl-redirect');
 var session = require('express-session');
 var pgSession = require('connect-pg-simple')(session);
 var db = require('./database/connect-db');
 
 var app = express();
+
+app.use(sslRedirect());
 
 app.use(session({
   store: new pgSession({
